06d57f8594e20ef4caa1445bef906f53209d5f0a
KirianCaumes <kirian.caumes@ynov.com>
2020-12-18 10:34:15 +0100

    First test heroku

--- back/index.js
+++ back/index.js
@@ -3,9 +3,16 @@ import express from 'express';
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
+    res.send('Hello World!')
     res.send('Hello World!')
 });
+
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(path.resolve(), '/client')))
+app.get('*', (req, res) => res.sendFile(path.join(path.resolve() + '/client/index.html')))
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
