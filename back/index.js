878fe7900ae5d435aae4d429ba209063ba0eec0c
KirianCaumes <kirian.caumes@ynov.com>
2020-12-18 10:32:50 +0100

    Revert "First test heroku"
    This reverts commit cc193acd21e22ed8d123a53b1f84e6a8d45dbd21.

--- back/index.js
+++ back/index.js
@@ -1,16 +1,11 @@
 import express from 'express';
-import path from 'path'
 
 const app = express();
 const port = 5000;
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 });
-
-// Serve static files from the React frontend app
-app.use(express.static(path.join(path.resolve(), '/client')))
-app.get('*', (req, res) => res.sendFile(path.join(path.resolve() + '/client/index.html')))
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
